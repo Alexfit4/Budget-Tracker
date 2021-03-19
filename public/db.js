0ab065753c4ebf9cabe28949cb0e8def216c5cd2
alexfit4 <alex.ashtiany@gmail.com>
2021-03-19 16:51:02 -0400

    updated routes

--- public/db.js
+++ public/db.js
@@ -32,38 +32,6 @@ function saveRecord(record) {
   store.add(record);
 }
 
-function checkDatabase() {
-  // open a transaction on your transactions db
-  const transaction = db.transaction(["transactions"], "readwrite");
-  // access your transactions object store
-  const store = transaction.objectStore("transactions");
-  // get all records from store and set to a variable
-  const getAll = store.getAll();
-
-  getAll.onsuccess = function() {
-    if (getAll.result.length > 0) {
-      fetch("/api/transaction/bulk", {
-        method: "POST",
-        body: JSON.stringify(getAll.result),
-        headers: {
-          Accept: "application/json, text/plain, */*",
-          "Content-Type": "application/json"
-        }
-      })
-      .then(response => response.json())
-      .then(() => {
-        // if successful, open a transaction on your transactions db
-        const transaction = db.transaction(["transactions"], "readwrite");
-
-        // access your transactions object store
-        const store = transaction.objectStore("transactions");
-
-        // clear all items in your store
-        store.clear();
-      });
-    }
-  };
-}
 
 // listen for app coming back online
 window.addEventListener("online", checkDatabase);
